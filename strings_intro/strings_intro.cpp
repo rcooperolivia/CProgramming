ddb3aadb81866f060cae5484755b3b22fab7a7d9
Olivia Cooper <ocooper7686@floridapoly.edu>
2026-01-16 18:47:23 +0000

    -m "Create String Program"

---
+++ strings_intro/strings_intro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+    // create a string
+    std::string str = "Hello";
+    
+    std::cout << str << std::endl;
+    std::cout << "the size of the string is " << str.size() << std::endl;
+
+    // concentrate strings
+   // str += " World!";
+    //std::cout 
+
+
+    return 0;
+}
