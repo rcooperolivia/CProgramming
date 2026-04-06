289d3cf69c57409a57064bf17ee31cb28f5af49b
Olivia Cooper <ocooper7686@floridapoly.edu>
2026-04-06 12:12:05 -0400

    Update Point.cpp

--- 23_overloading_something/Point.cpp
+++ 23_overloading_something/Point.cpp
@@ -1,53 +1,56 @@
 #include "Point.hpp"
-using namespace std;
-#include <iostream>
 
-Point::Point(int x, int y): x(x), y(y){
-    //
+Point::Point(int x, int y) : x(x), y(y) {
+
 }
 
-//relational operators
-bool Point::operator==(const Point& other){
+bool Point::operator==(const Point& other) {
     return (x == other.x && y == other.y);
 }
-bool Point::operator!=(const Point& other){
+
+bool Point::operator!=(const Point& other) {
     return !(*this == other);
 }
 
-//bracket operator
-int& Point::operator[](int index){
+int& Point::operator[](int index) {
     return (index == 0) ? x : y;
 }
 
-// //aritmetic operator
-// Point Point::operator+(const Point& other) const{
-//     return Point (x + other.x, y + other.y);
-// }
 
+std::string Point::toString() const {
+    return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+}
 
-//Arithmatic assignmemt operator
-Point& operator+=(const Point& other){
-    // //option 1
+Point Point::operator+(const Point& other) const {
+    return Point(x + other.x, y + other.y);
+}
+
+Point& Point::operator+=(const Point& other) {
+    // // Option 1
     // x += other.x;
     // y += other.y;
 
-    // return *this;
 
-    // //option 2
-    
-    // //copy of current object
-    // Point tmp(x,y);
+    // return *this;
+    // // Option 2 Copy of the current object
+    // Point tmp(x, y);
     // Point tmp2(*this);
 
     // return tmp;
     // return tmp2;
 
-    //option 3 
+    // Option 3.
     *this = *this + other;
+    return *this;
+
+}
+
+Point Point::operator++() {
+    x++;
+    y++;
     return *this;
 }
 
-//to string
-std::string Point::toString() const{
-    return "(" + std::to_string(x) + std::to_string(y) + ")";
+Point Point::operator++(int) {
+    return Point(x++, y++);
 }
