d8a7e8f3bcd6b18b37b260494fe019ba22185733
Olivia Cooper <ocooper7686@floridapoly.edu>
2026-04-08 12:35:07 -0400

    Update Point.cpp

--- 23_overloading_something/Point.cpp
+++ 23_overloading_something/Point.cpp
@@ -2,36 +2,43 @@
 #include <cstring>
 #include <iostream>
 
-Point::Point(int x, int y, const char* new_tag) : x(x), y(y) {
-    if (new_tag){
+Point::Point(int x, int y, const char* new_tag) {
+    this->x = x;
+    this->y = y;
+    if (new_tag) {
         int size = strlen(new_tag);
         tag = new char[size + 1];
         strcpy(tag, new_tag);
     }
-    else{
+    else {
         tag = nullptr;
     }
-
 }
+
+Point::~Point() {
+    if (tag) {
+        delete [] tag;
+    }
+}
+
 Point::Point(const Point& other) {
     x = other.x;
-    y == other.y;
-    if (other.tag){
+    y = other.y;
+    if (other.tag) {
         int size = strlen(other.tag);
         tag = new char[size + 1];
         strcpy(tag, other.tag);
     }
-    else{
+    else {
         tag = nullptr;
     }
-
 }
 
-bool Point::operator==(const Point& other) {
+bool Point::operator==(const Point& other) const {
     return (x == other.x && y == other.y);
 }
 
-bool Point::operator!=(const Point& other) {
+bool Point::operator!=(const Point& other) const {
     return !(*this == other);
 }
 
@@ -39,13 +46,13 @@ int& Point::operator[](int index) {
     return (index == 0) ? x : y;
 }
 
-
 std::string Point::toString() const {
+    std::string s;
     if (tag) {
         s += std::string(tag);
     }
-    return s + "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
-    
+
+    return s + ": (" + std::to_string(x) + ", " + std::to_string(y) + ")";
 }
 
 Point Point::operator+(const Point& other) const {
@@ -53,97 +60,53 @@ Point Point::operator+(const Point& other) const {
 }
 
 Point& Point::operator+=(const Point& other) {
-    // // Option 1
-    // x += other.x;
-    // y += other.y;
-
-
-    // return *this;
-    // // Option 2 Copy of the current object
-    // Point tmp(x, y);
-    // Point tmp2(*this);
-
-    // return tmp;
-    // return tmp2;
-
-    // Option 3.
     *this = *this + other;
     return *this;
-
 }
 
 Point Point::operator++() {
-    x++;
-    y++;
+    x += 1;
+    y += 1;
     return *this;
 }
 
 Point Point::operator++(int) {
-    return Point(x++, y++);
+    Point temp(*this);
+    x += 1;
+    y += 1;
+    return temp;
+    // return Point(x++, y++);
 }
 
-Point Point::operator*(const Point& other) const{
-    for (i= 0; i< other.y; i++){
-        y= y + y;
-    } 
-    for (i= 0; i< other.x; i++){
-        x= x + x;
-    } 
-    return *this;
-} // TODO (can you use + ?)
-
-Point Point::operator-(const Point& other) const{
-    return Point(x - other.x, y - other.y);
-} // TODO
-
-Point& Point::operator*=(const Point& other){
-    *this = *this * other;
-    return *this;
-} // TODO
-
-Point& Point::operator-=(const Point& other){
-    *this = *this - other;
-    return *this;
-} // TODO
-
-
-//destructor
-~Point::Point(){
-    if (tag){
+Point& Point::operator=(const Point& other) {
+    x = other.x;
+    y = other.y;
+    // Deallocate old memory
+    if (tag) {
         delete [] tag;
     }
-}
-
-
-//assignmemt operator
-Point& Point::operator=(const Point& other){
-    x = other.x;
-    y == other.y;
-    if (other.tag){
+    if (other.tag) {
         int size = strlen(other.tag);
         tag = new char[size + 1];
         strcpy(tag, other.tag);
     }
-    else{
+    else {
         tag = nullptr;
-    }
+    } 
+
+    return *this;
 }
 
-
-std::ostream operator<<(std::ostream& out, const Point& p){
+std::ostream& operator<<(std::ostream& out, const Point& p) {
     out << p.toString();
     return out;
 }
 
-//friend method 
-friend std::istream operator>>(std::istream& in, Point& p){
-    std::cout<< "Enter x, y, tag ";
+std::istream& operator>>(std::istream& in, Point& p) {
+    std::cout << "Enter x, y, tag ";
     in >> p.x;
     in >> p.y;
-    //TODO get tag
-    if (p.tag){
-        delete [] tag;
-    }
+    // TODO get the tag
+
     return in;
 }
-
