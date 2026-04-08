a3162609645faccd652425398721ef87abe00288
Olivia Cooper <ocooper7686@floridapoly.edu>
2026-04-08 12:34:06 -0400

    Update mainPoint.cpp

--- 23_overloading_something/mainPoint.cpp
+++ 23_overloading_something/mainPoint.cpp
@@ -2,28 +2,24 @@
 #include <iostream>
 
 int main(void) {
-    // Point p1(3, 4);
-    // Point p2(5, 4);
-    // Point p3(81, 82);
+    Point p1(3, 4, "my point");
 
-    // std::cout << "p3 " << p3.toString() << std::endl;
-    
-    // Point p4 = p1 + p2;
-    
-    // std::cout << "p4 " << p4.toString() << std::endl;
-    
-    
-    // int x = 1;
-    // (x += 2) += 5;
-    // std::cout << x << std::endl;
-    
-    // (p4 += p1) += p2;
-    // std::cout << "p4 " << p4.toString() << std::endl;
-    
-    // p3 = p4++;
-    // std::cout << "p3 " << p4.toString() << std::endl;
+    Point p2(p1);
+    Point p3;
 
-    Point p1(3, 4, "my point: ");
+    Point p4(1, 1, "p4");
+
+    p3 = p1 = p4;
+
     std::cout << p1.toString() << std::endl;
+    std::cout << p2.toString() << std::endl;
+    std::cout << p3.toString() << std::endl;
+    std::cout << p4.toString() << std::endl;
+    
+    std::cout << "---" << p1 << std::endl;
+
+    std::cin >> p4;
+    std::cout << p4;
+
     return 0;
 }
