f252eea3be15a6af7ea7e41ddc103d59a5f23202
Olivia Cooper <ocooper7686@floridapoly.edu>
2026-04-27 13:03:12 -0400

    Update exceptions.cpp

--- 29_exceptions/exceptions.cpp
+++ 29_exceptions/exceptions.cpp
@@ -1,75 +1,40 @@
 #include <iostream>
-#include <stdexcept>
-#include <string>
-using namespace std;
 
-class MyException : public std::logic_error {
-    public:
-    MyException(int code, const std::string& msg) : std::logic_error(msg), err_code(code){
-    }
+class Fraction {
+public:
+    Fraction(int n, int d) : num(n), den(d) {
 
-    private:
-    int err_code;
-
-    protected:
-    //
-};
-
-template <typename T>
-class Fraction{
-    public:
-    Fraction(int n = 1, int d = 1) : num(n), den(d){
-        //
     }
 
     int quotient() const {
-        if (den == 0){
-            throw std::runtime_error("Denominator can not be 0");
+        if (den == 0) {
+            throw "Denominator cannot be zero.";
         }
-        // else if (num > 99) {
-        //     throw 1; // what if i don't like big numbers
-        // }
-        else if ((0 > num && den > 0;) || (0 > den && num > 0;)){
-            throw std::logic_error("Don't do that");
-        }
+        throw 1;
 
         return num / den;
     }
-
-    private:
+private:
     int num, den;
-
-    protected:
-    //
 };
 
-int main(void){
+int main(void) {
     int n1, n2;
-    cout << "Enter two integers: ";
-    cin >> n1 >> n2;
+    std::cout << "Enter two integers: ";
+    std::cin >> n1 >> n2;
 
-    Fraction f1(n1,n2);
-    
-    //try block
+    Fraction f1(n1, n2);
+
     try {
-        cout << f1.quotient() << endl;
-        cout << "End of try block" << endl;
+        std::cout << f1.quotient() << std::endl;
+        std::cout << "End of try block" << std::endl;
     }
-    //catch block
-    catch (const std::logic_error& ex){
-        cout << "Logic error was caught: " << what << endl; //what return message in constructor
+    catch (const char* ex) {
+        std::cout << "Char* exception was catched: " << ex << std::endl;
     }
-    //second catch...
-    catch (const std::runtime_error& ex){
-        cout << "Runtime error was caught with the code: " << what << endl;
-    }
-    //lmao whatevs
-    // catch (...){
 
-    // }
+    std::cout << "End of program" << std::endl;
 
-
-    cout << "End of program" << endl;
-
+    
     return 0;
 }
